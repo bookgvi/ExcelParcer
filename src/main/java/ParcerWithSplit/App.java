ae2c71e607e1c8460085d406d21a49cdb4e0095c
bookgvi <bookgvi@gmail.com>
2021-05-02 12:31:28 +0300

    split string into ArrayList V2 method

--- src/main/java/ParcerWithSplit/App.java
+++ src/main/java/ParcerWithSplit/App.java
@@ -9,7 +9,9 @@ public class App {
         String fileName = FilesAndDirectories.inputFileDirectory +  "PCG-55820_april_v2.xlsx";
         XLSUtils xlsObj = new XLSUtils(fileName);
         ArrayList<ArrayList<String>> data = xlsObj.parce(3);
-        ArrayList<ArrayList<String>> flatData = new SerializeUtils(data).flatArraylist();
-        xlsObj.createNewSheet(flatData);
+//        ArrayList<ArrayList<String>> flatData = new SerializeUtils(data).splitToArraylistV1();
+        ArrayList<ArrayList<String>> flatData2 = new SerializeUtils(data).splitToArraylistV2();
+//        xlsObj.createNewSheet(flatData);
+        xlsObj.createNewSheet(flatData2);
     }
 }
--- src/main/java/ParcerWithSplit/SerializeUtils.java
+++ src/main/java/ParcerWithSplit/SerializeUtils.java
@@ -1,18 +1,20 @@
 package ParcerWithSplit;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SerializeUtils {
-    ArrayList<ArrayList<String>> _dataList;
+    private final ArrayList<ArrayList<String>> _dataList;
 
     public SerializeUtils(ArrayList<ArrayList<String>> dataList) {
         _dataList = (ArrayList<ArrayList<String>>) dataList.clone();
     }
 
-    public ArrayList<ArrayList<String>> flatArraylist() {
+    public ArrayList<ArrayList<String>> splitToArraylistV1() {
         ArrayList<ArrayList<String>> newDataList = new ArrayList<>();
         IntStream.range(0, _dataList.size())
                 .forEach(index -> {
@@ -31,7 +33,8 @@ public class SerializeUtils {
                         ArrayList<String> tmpList = new ArrayList<>();
                         if (firstInsideEltArr.get().length() > index2) tmpList.add(firstInsideEltArr.get().get(index2));
                         else tmpList.add(firstInsideEltArr.get().get(firstInsideEltArr.get().length() - 1));
-                        if (secondInsideEltArr.get().length() > index2) tmpList.add(secondInsideEltArr.get().get(index2));
+                        if (secondInsideEltArr.get().length() > index2)
+                            tmpList.add(secondInsideEltArr.get().get(index2));
                         else tmpList.add(secondInsideEltArr.get().get(secondInsideEltArr.get().length() - 1));
                         if (thirdInsideEltArr.get().length() > index2) tmpList.add(thirdInsideEltArr.get().get(index2));
                         else tmpList.add(thirdInsideEltArr.get().get(thirdInsideEltArr.get().length() - 1));
@@ -39,5 +42,45 @@ public class SerializeUtils {
                     });
                 });
         return newDataList;
+    }
+
+    public ArrayList<ArrayList<String>> splitToArraylistV2() {
+        final int defaultColumnsCount = 3;
+        return splitToArraylistV2(defaultColumnsCount);
+    }
+
+    public ArrayList<ArrayList<String>> splitToArraylistV2(int columnsCount) {
+        ArrayList<ArrayList<String>> newDataList = new ArrayList<>();
+        IntStream.range(0, _dataList.size())
+                .forEach(index -> {
+                    ArrayList<String> nestedArrayList = _dataList.get(index);
+                    List<String[]> splitArrayHolder = this.splitString(columnsCount, nestedArrayList, "\\|");
+                    int maxSize = getMaxSizeOfArrays(splitArrayHolder);
+                    joinArray(splitArrayHolder, maxSize, newDataList);
+                });
+        return newDataList;
+    }
+
+    private List<String[]> splitString(int columnsCount, ArrayList<String> arrayList, String delimeter) {
+        return IntStream.range(0, columnsCount)
+                .mapToObj(i -> arrayList.get(i).split(delimeter)).collect(Collectors.toList());
+    }
+
+    private int getMaxSizeOfArrays(List<String[]> streamWithArrays) {
+        List<Integer> listWithArrays = streamWithArrays.stream().map(arr -> arr.length).sorted().collect(Collectors.toList());
+        return listWithArrays.get(listWithArrays.size() - 1);
+    }
+
+    private ArrayList<ArrayList<String>> joinArray(List<String[]> splitArrayHolder, int maxSize, ArrayList<ArrayList<String>> res) {
+        IntStream.range(0, maxSize).forEach(i -> {
+            ArrayList<String> tmpArr = new ArrayList<>();
+            splitArrayHolder.forEach(array -> {
+                if (array.length > i) tmpArr.add(array[i]);
+                else if (array.length == 1) tmpArr.add(array[0]);
+                else tmpArr.add("-");
+            });
+            res.add(tmpArr);
+        });
+        return res;
     }
 }
